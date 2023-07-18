8866cc6d018473ac587454a1b978786d161bf291
username <128367359+shadowaxe99@users.noreply.github.com>
2023-07-18 11:14:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src.agent_interaction import AgentInteraction
 
 if __name__ == '__main__':
-    ai = AgentInteraction()
-    ai.initialize_project('Test Project', 'Test Objectives')
+    try:
+        ai = AgentInteraction()
+        ai.initialize_project('Test Project', 'Test Objectives')
+    except Exception as e:
+        print('Error: ' + str(e))
+        sys.exit(1)
 
 print('Initialized project')
