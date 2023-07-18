c8250e492cc418dd3d6af53c5c90e4d2ece20bd8
username <128367359+shadowaxe99@users.noreply.github.com>
2023-07-18 07:23:33 -0400

    Updated main.py with debug print statement

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from src.agent_interaction import AgentInteraction
 if __name__ == '__main__':
     ai = AgentInteraction()
     ai.initialize_project('Test Project', 'Test Objectives')
+
+print('Initialized project')
